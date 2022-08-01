01a81e925ac68f93e1788aa91f51fe73347ab091
Jerry applegarth <applegarth1630@gmail.com>
2022-08-01 12:39:12 -0700

    added four text files and save button

--- app/src/main/java/com/applecompose/theplant/MainActivity.kt
+++ app/src/main/java/com/applecompose/theplant/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.applecompose.theplant
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.applecompose.theplant.ui.theme.ThePlantTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
 					modifier = Modifier.fillMaxSize(),
 					color = MaterialTheme.colors.background
 				) {
-					Greeting("Android")
+					SpecimenFacts()
 				}
 			}
 		}
@@ -30,14 +35,68 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-	Text(text = "Hello $name!")
+fun SpecimenFacts() {
+	var plantName by remember { mutableStateOf("") }
+	var description by remember { mutableStateOf("")}
+	var location by remember { mutableStateOf("")}
+	var datePlanted by remember { mutableStateOf("")}
+	val context = LocalContext.current
+
+	Column(
+		modifier = Modifier
+			.fillMaxWidth()
+			.padding(12.dp)
+	) {
+		OutlinedTextField(
+			value = plantName,
+			onValueChange = {plantName = it},
+			label = { Text( stringResource(R.string.plant_name))},
+			modifier = Modifier
+				.fillMaxWidth()
+		)
+		OutlinedTextField(
+			value = location,
+			onValueChange = {location = it},
+			label = { Text( stringResource(R.string.location))},
+			modifier = Modifier
+				.fillMaxWidth()
+		)
+		OutlinedTextField(
+			value = description,
+			onValueChange = {description = it},
+			label = { Text( stringResource(R.string.description))},
+			modifier = Modifier
+				.fillMaxWidth()
+		)
+		OutlinedTextField(
+			value = datePlanted,
+			onValueChange = {datePlanted = it},
+			label = { Text( stringResource(R.string.date_planted))},
+			modifier = Modifier
+				.fillMaxWidth()
+		)
+		Button(onClick = {
+			// code goes here
+			Toast.makeText(context,
+				"You have SAVED $plantName, " +
+						"$location, " +
+						"$description, " +
+						"$datePlanted, $",
+				Toast.LENGTH_LONG).show()
+
+		}) {
+			Text(text = "Save")
+		}
+
+	}
+
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
 	ThePlantTheme {
-		Greeting("Android")
+		SpecimenFacts()
 	}
 }
