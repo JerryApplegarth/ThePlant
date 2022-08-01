23acd1023ba4cdf9e2ecae6cd71212dec2401456
Jerry applegarth <applegarth1630@gmail.com>
2022-08-01 15:38:55 -0700

    added colors to theme

--- app/src/main/java/com/applecompose/theplant/MainActivity.kt
+++ app/src/main/java/com/applecompose/theplant/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.applecompose.theplant
 
+import android.content.res.Configuration
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.key.Key.Companion.U
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.applecompose.theplant.ui.theme.ThePlantTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,7 +28,7 @@ class MainActivity : ComponentActivity() {
 			ThePlantTheme {
 				// A surface container using the 'background' color from the theme
 				Surface(
-					modifier = Modifier.fillMaxSize(),
+					modifier = Modifier.fillMaxWidth(),
 					color = MaterialTheme.colors.background
 				) {
 					SpecimenFacts()
@@ -37,9 +41,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun SpecimenFacts() {
 	var plantName by remember { mutableStateOf("") }
-	var description by remember { mutableStateOf("")}
-	var location by remember { mutableStateOf("")}
-	var datePlanted by remember { mutableStateOf("")}
+	var description by remember { mutableStateOf("") }
+	var location by remember { mutableStateOf("") }
+	var datePlanted by remember { mutableStateOf("") }
 	val context = LocalContext.current
 
 	Column(
@@ -47,56 +51,90 @@ fun SpecimenFacts() {
 			.fillMaxWidth()
 			.padding(12.dp)
 	) {
-		OutlinedTextField(
-			value = plantName,
-			onValueChange = {plantName = it},
-			label = { Text( stringResource(R.string.plant_name))},
-			modifier = Modifier
-				.fillMaxWidth()
-		)
-		OutlinedTextField(
-			value = location,
-			onValueChange = {location = it},
-			label = { Text( stringResource(R.string.location))},
-			modifier = Modifier
-				.fillMaxWidth()
-		)
-		OutlinedTextField(
-			value = description,
-			onValueChange = {description = it},
-			label = { Text( stringResource(R.string.description))},
-			modifier = Modifier
-				.fillMaxWidth()
-		)
-		OutlinedTextField(
-			value = datePlanted,
-			onValueChange = {datePlanted = it},
-			label = { Text( stringResource(R.string.date_planted))},
-			modifier = Modifier
-				.fillMaxWidth()
-		)
-		Button(onClick = {
-			// code goes here
-			Toast.makeText(context,
-				"You have SAVED $plantName, " +
-						"$location, " +
-						"$description, " +
-						"$datePlanted, $",
-				Toast.LENGTH_LONG).show()
+		TopAppBar(
 
-		}) {
-			Text(text = "Save")
+		) {
+			Text(
+				text = "The Plant",
+				fontSize = 20.sp,
+				modifier = Modifier
+					.padding(16.dp),
+
+				)
 		}
+		Column(
+			modifier = Modifier
+				.fillMaxWidth()
+		) {
 
+
+			OutlinedTextField(
+				value = plantName,
+				onValueChange = { plantName = it },
+				label = { Text(stringResource(R.string.plant_name)) },
+				modifier = Modifier
+					.fillMaxWidth()
+			)
+			OutlinedTextField(
+				value = location,
+				onValueChange = { location = it },
+				label = { Text(stringResource(R.string.location)) },
+				modifier = Modifier
+					.fillMaxWidth()
+			)
+			OutlinedTextField(
+				value = description,
+				onValueChange = { description = it },
+				label = { Text(stringResource(R.string.description)) },
+				modifier = Modifier
+					.fillMaxWidth()
+			)
+			OutlinedTextField(
+				value = datePlanted,
+				onValueChange = { datePlanted = it },
+				label = { Text(stringResource(R.string.date_planted)) },
+				modifier = Modifier
+					.fillMaxWidth()
+			)
+			Button(onClick = {
+				// code goes here
+				Toast.makeText(
+					context,
+					"You have SAVED $plantName, " +
+							"$location, " +
+							"$description, " +
+							"$datePlanted, $",
+					Toast.LENGTH_LONG
+				).show()
+
+			}) {
+				Text(text = "Save")
+			}
+		}
 	}
 
 
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+@Preview(
+	showBackground = true,
+	//showSystemUi = true,
+	name = "Light Model"
+)
+@Preview(
+	showBackground = true,
+	//showSystemUi = true,
+	name = "Dark Mode",
+	uiMode = Configuration.UI_MODE_NIGHT_YES
+)
 @Composable
-fun DefaultPreview() {
+fun DefaultPreviewDark() {
 	ThePlantTheme {
-		SpecimenFacts()
+		Surface(
+			color = MaterialTheme.colors.background,
+			modifier = Modifier.fillMaxWidth(),
+		) {
+			SpecimenFacts()
+		}
 	}
 }
--- app/src/main/java/com/applecompose/theplant/ui/theme/Theme.kt
+++ app/src/main/java/com/applecompose/theplant/ui/theme/Theme.kt
@@ -5,21 +5,24 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
-	primary = Purple200,
+	primary = DarkSecondary,
 	primaryVariant = Purple700,
 	secondary = Teal200
 )
 
 private val LightColorPalette = lightColors(
-	primary = Purple500,
-	primaryVariant = Purple700,
-	secondary = Teal200
+	primary = DkRed,
+	primaryVariant = DarkSecondary,
+	secondary = Tan,
+	background = Tan,
+	surface = Color.Black
 
 	/* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+
+
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
